0e0915ac33c2a7c67ba0cdda83a3c55ac908aea2
Chirag Arora <54665586+TheChirag356@users.noreply.github.com>
2023-09-28 21:09:43 +0530

    Added labels
    Added text label and aligned it

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class Main {
@@ -19,12 +20,29 @@ public class Main {
 
 //        MyFrame myframe = new MyFrame();
 
-        ImageIcon image = new ImageIcon("assets/logo.png");
+        ImageIcon imageicon = new ImageIcon("assets/logo.png");
+        Image image = imageicon.getImage();
+        Image newimg = image.getScaledInstance(120, 120, Image.SCALE_SMOOTH);
+        imageicon = new ImageIcon(newimg);
+
+        Border border = BorderFactory.createLineBorder(Color.green, 3);
 
         JLabel label = new JLabel();// create a label
         label.setText("Bro, do you even code?"); // set text of label
-        label.setIcon(image);0
+        label.setIcon(imageicon);
         label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setForeground(new Color(0x00FF00));
+        label.setFont(new Font("Ubuntu", Font.PLAIN, 20));
+        label.setIconTextGap(-10);
+        label.setBackground(Color.BLACK);
+        label.setOpaque(true);
+        label.setBorder(border);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
+
+
+
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
